265341fdda3b14d591e60c02cf0d0f93f9fb1c19
Jurga Gi <jurga.gird@gmail.com>
2023-12-28 00:05:34 +0200

    Added dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vaiko-vardas-page/',
   plugins: [vue(), viteCompression()],
 });
